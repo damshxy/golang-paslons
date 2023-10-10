7bcae4bc4f03c0498e9bace31b9337365d814f36
Damshxy <mrsaddam2806@gmail.com>
2023-10-10 21:31:10 +0700

    nothing happen

--- database/cloudinary.database.go
+++ database/cloudinary.database.go
@@ -8,33 +8,34 @@ import (
 )
 
 func EnvCloudName() string {
-		err := godotenv.Load()
-		if err != nil {
-				log.Fatal("Error loading .env file")
-		}
-		return os.Getenv("CLOUDINARY_CLOUD_NAME")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	return os.Getenv("CLOUDINARY_CLOUD_NAME")
 }
 
 func EnvCloudAPIKey() string {
-		err := godotenv.Load()
-		if err != nil {
-				log.Fatal("Error loading .env file")
-		}
-		return os.Getenv("CLOUDINARY_API_KEY")
+	err := godotenv.Load()
+	if err != nil {
+			log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_API_KEY")
 }
 
 func EnvCloudAPISecret() string {
-		err := godotenv.Load()
-		if err != nil {
-				log.Fatal("Error loading .env file")
-		}
-		return os.Getenv("CLOUDINARY_API_SECRET")
+	err := godotenv.Load()
+	if err != nil {
+			log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_API_SECRET")
 }
 
 func EnvCloudUploadFolder() string {
-		err := godotenv.Load()
-		if err != nil {
-				log.Fatal("Error loading .env file")
-		}
-		return os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
+	err := godotenv.Load()
+	if err != nil {
+			log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
 }
